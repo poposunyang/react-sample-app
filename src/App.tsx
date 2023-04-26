0616162363c14802b9c4cdbe436656a60c4ae55f
poposunyang <poposunyang@gmail.com>
2023-04-26 15:24:17 +0900

    course3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./App.css";
+import { User } from "./User";
 
 const App: React.FunctionComponent = () => {
+  const users = [
+    { name: "sunyang", age: 37 },
+    { name: "maoyu", age: 34 },
+  ];
   return (
     <div className="App">
-      <h1>Hello</h1>
+      {users.map((user, key) => {
+        return (
+          <User key={key} keyindex={key} name={user.name} age={user.age} />
+        );
+      })}
     </div>
   );
 };
