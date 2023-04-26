e5d3c1055f9656f1265e39b45bb501fb92f47fef
poposunyang <poposunyang@gmail.com>
2023-04-26 14:13:59 +0900

    Course2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./App.css";
 
+interface User {
+  name: string;
+  age: number;
+  email: string;
+}
+
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <UserComponent name="Sun" age={1} email="Sun@" />
+    </div>
+  );
+};
+
+const UserComponent = (props: User): JSX.Element => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
     </div>
   );
 };
